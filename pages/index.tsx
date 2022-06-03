4f44c57416df0045e28742101eaf33e902c8cd27
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2022-06-03 22:08:06 +0200

    navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from 'react'
 import Commerce from '@chec/commerce.js';
+import Navbar from '../components/navbar'
 
 const commerce = new Commerce(process.env.NEXT_PUBLIC_CHEC_PUBLIC_KEY, true);
-export default function Home() {
-    //products
-    const [p, sp] = useState([]);
-    //loading
-    const [l, sl] = useState(true);
-    useEffect(() => { async () => await sp(commerce.products.list()); sl(false) })
+export default function a() {
+
+    const [products, setProducts] = useState([]);
+
+    const [loading, setLoading] = useState(true);
+    useEffect(() => { async () => await setProducts(commerce.products.list()); setLoading(false) })
     return (<>
-        <div className="navbar"></div>
+        <Navbar></Navbar>
 
 
-        {l ? <h1>... loading</h1> : <><h1>Our Products</h1>{p.map((product) => { <li>uu</li> })}</>}
+        {loading ? <h1>... loading</h1> : <><h1>Our Products</h1>{products.map((product) => { <li>uu</li> })}</>}
     </>
     )
 
