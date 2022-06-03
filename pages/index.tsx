a6180f934e1c81c63793e92ee9a61dea4e419403
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2022-06-03 21:50:12 +0200

    empty list

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,16 @@ import Commerce from '@chec/commerce.js';
 
 const commerce = new Commerce(process.env.NEXT_PUBLIC_CHEC_PUBLIC_KEY, true);
 export default function Home() {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => { async () => await setProducts(commerce.products.list()); })
+    //products
+    const [p, sp] = useState([]);
+    //loading
+    const [l, sl] = useState(true);
+    useEffect(() => { async () => await sp(commerce.products.list()); sl(false) })
     return (<>
         <div className="navbar"></div>
 
 
-        {products.map((product) => { <li>uu</li> })}
+        {l ? <h1>... loading</h1> : <><h1>Our Products</h1>{p.map((product) => { <li>uu</li> })}</>}
     </>
     )
 
