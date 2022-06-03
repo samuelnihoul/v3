29ed1f7d969adb1d325e08c4865a857db4904ae5
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2022-06-03 12:17:26 +0200

    housekeeping

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import { useState, onEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Commerce from '@chec/commerce.js';
 
 const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
 export const Home = () => {
     const [products, setProducts] = useState([]);
-    onEffect(() => { setProducts(await commerce.products.list()) })
+
+    useEffect(() => { async () => await setProducts(commerce.products.list()); })
     return (<>
         <div className="navbar"></div>
 
